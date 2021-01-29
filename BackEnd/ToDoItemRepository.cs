15785e6d0cb51192684aedb625542add4a405052
Stina <christinama92@gmail.com>
2021-01-29 10:45:13 +0000

    todoitem repo

--- BackEnd/ToDoItemRepository.cs
+++ BackEnd/ToDoItemRepository.cs
@@ -1,4 +1,41 @@
+using System.Collections.Generic;
+using Dapper;
+using System;
 
+public class ToDoItemRepository : BaseRepository
+{
+    public IEnumerable<ToDoItem> GetAll()
+    {
+        using var connection = CreateConnection();
+        return connection.Query<ToDoItem> ("SELECT * FROM ToDoItems;");
+    }
+
+    public ToDoItem GetOne(long id)
+    {
+        using var connection = CreateConnection();
+        return connection.QuerySingle<ToDoItem>("SELECT * FROM ToDoItems WHERE id=@Id;", new{Id = id});
+    }
+
+    public void Delete(long id)
+    {
+        using var connection = CreateConnection();
+        connection.Execute("DELETE FROM ToDoItems WHERE id =@Id;", new{Id = id});
+    }
+
+    public ToDoItem Update(ToDoItem toDoItem)
+    {
+        using var connection = CreateConnection();
+        connection.QuerySingle<ToDoItem>("UPDATE ToDoItems SET Title =@Title, Priority =@Prioirity IsComplete =@IsComplete WHERE id=@Id RETURNING Id, Title, Priority, IsComplete;", toDoItem);
+
+    }
+
+    public ToDoItem Create(ToDoItem toDoItem)
+    {
+        using var connection = CreateConnection();
+        connection.QuerySingle<ToDoItem>("INSERT INTO ToDoItems (Title, Priority, IsComplete) VALUES (@Title, @Priority, @IsComplete) RETURNING Id, Title, Priority, IsComplete;", toDoItem);
+    }
+
+}
 
 //GetAll
 //GetOne
