54b7f62cd46028a48c71de2e0a34be4448f2d690
OanaMG <oana_ghidiiu@yahoo.com>
2021-01-29 11:40:42 +0200

    object

---
+++ BackEnd/ToDoItem.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class ToDoItem {
+
+    public long? Id {get; set;}
+    public string Title {get; set;}
+    public string Priority {get; set;}
+    public bool IsComplete {get; set;}
+}
