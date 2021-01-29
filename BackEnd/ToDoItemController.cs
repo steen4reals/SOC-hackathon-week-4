da36e0f9f732cfe5f699211abcd21a495bdce094
Stina <christinama92@gmail.com>
2021-01-29 10:17:57 +0000

    controllers

--- BackEnd/ToDoItemController.cs
+++ BackEnd/ToDoItemController.cs
@@ -12,16 +12,39 @@ public class ToDoItemController : ControllerBase {
         _toDoItemRepository = new ToDoItemRepository ();
     }
 
-    [HTTPGet]
+    [HttpGet]
     public IEnumerable<ToDoItem> GetAllItems() {  //if it doesn't work add the id
 
         return _toDoItemRepository.GetAll();  
     }
 
     [HttpGet("{Id}")]
-    public ToDoItem GetAllItems(long id) {  
+    public ToDoItem GetOneItem(long id) {  
 
         return _toDoItemRepository.GetOne(id);  
     }
+
+    [HttpDelete("{Id}")]
+    public void DeleteItem(long id)
+    {
+        _toDoItemRepository.Delete(id);
+    }
+
+    [HttpPut("{Id}")]
+
+    public ToDoItem UpdateItem(long id, [FromBody] ToDoItem toDoItem)
+    {
+        toDoItem.Id = id;
+        return _toDoItemRepository.Update(toDoItem);
+    }
+
+    [HttpPost]
+
+    public ToDoItem CreateItem([FromBody] ToDoItem toDoItem)
+    {
+        return _toDoItemRepository.Create(toDoItem);
+    }
+
+
 
 }
--- BackEnd/ToDoItemRepository.cs
+++ BackEnd/ToDoItemRepository.cs
@@ -2,3 +2,6 @@
 
 //GetAll
 //GetOne
+//Delete
+//Update
+//Create
